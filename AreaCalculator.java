686871caaaa5dfa30c0a58dd2d34ac11d4258dd3
jclopezr30 <julissa.trab30@gmail.com>
2023-03-14 17:47:51 -0600

    Clase Triangulo

--- AreaCalculator.java
+++ AreaCalculator.java
@@ -38,12 +38,7 @@ public class AreaCalculator {
                 break;
 
             case 4:
-                System.out.print("Ingresa la base del triángulo: ");
-                double baseTri = scanner.nextDouble();
-                System.out.print("Ingresa la altura del triángulo: ");
-                double heightTri = scanner.nextDouble();
-                double triangleArea = (baseTri * heightTri) / 2;
-                System.out.println("El área del triángulo es: " + triangleArea);
+                triangulo.calcular();
                 break;
 
             default:
