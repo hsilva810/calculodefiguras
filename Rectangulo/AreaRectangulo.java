9f58d7a75bba23fabf9f90f7feb19207ab5e5227
Marytecnonic <marytecnonic@gmail.com>
2023-03-14 18:05:16 -0600

    calculo rectangulo Manuel

---
+++ Rectangulo/AreaRectangulo.java
@@ -0,0 +1,15 @@
+package Rectangulo;
+
+import java.util.Scanner;
+
+public class AreaRectangulo {
+
+    public static void calcularAreaRectangulo () {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingresa la base del rectángulo: ");
+        double base = scanner.nextDouble();
+        System.out.print("Ingresa la altura del rectángulo: ");
+        double height = scanner.nextDouble();
+    }
+}
